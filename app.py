a3461023310589562a1992849593ee7db7663225
HarryJain <HarryJain99@gmail.com>
2023-04-26 03:21:42 -0400

    Fixed DB Engine URL

--- app.py
+++ app.py
@@ -27,9 +27,9 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 database_uri = 'postgresql://pohkbdxjkwmnms:288ae8a77dd3e18169c9fcf455e179425751e1eaf9bc77e95c63b442c48d3bce@ec2-44-214-9-130.compute-1.amazonaws.com:5432/d5imhhjosegjqo'
 app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
 
-engine = create_engine('postgresql://postgres:admin@localhost/thesis_nba')
+#engine = create_engine('postgresql://postgres:admin@localhost/thesis_nba')
 #engine = create_engine('postgres://snaqiuwvjxmcnv:87357d96f1e3322290c07dd3a0ac6b219655a3b9770641fcc7a7ce96694aff3d@ec2-44-199-22-207.compute-1.amazonaws.com:5432/d7npoqqmolsofp')
-#engine = create_engine(database_uri)
+engine = create_engine(database_uri)
 #engine = create_engine(database_uri)
 
 
